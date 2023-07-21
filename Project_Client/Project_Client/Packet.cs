7923de8b6a18b8923a53f7eeb01575c964fc8d31
KimDaeYoung42 <130177839+KimDaeYoung42@users.noreply.github.com>
2023-07-21 09:08:16 +0900

    0906_1900 기준

---
+++ Project_Client/Project_Client/Packet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Client
+{
+    internal class Packet
+    {
+        public const string Pack_Shortmessage = "P_SHORTMESSAGE"; 
+
+        public static string ShortMessage(string name, string msg)
+        {
+            string pack = string.Empty;
+
+            pack += Pack_Shortmessage + "@";
+            pack += name + "#";
+            pack += msg;
+
+            return pack;
+
+        }
+    }   
+}
